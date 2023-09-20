8f3bd2140ca5ac7d551b99100d160acf74af0323
csmplatform <csmplatform@users.noreply.github.com>
2023-09-20 09:20:24 +0000

    Automatically update java client (ref: Cosmo-Tech/cosmotech-api@09d26f3ac510a2867f366e676419bd5785121e81)
    Co-authored-by: jreynard-code <jreynard-code@users.noreply.github.com>

--- src/main/java/com/cosmotech/client/model/User.java
+++ src/main/java/com/cosmotech/client/model/User.java
@@ -31,7 +31,7 @@ import java.util.List;
  * a User
  */
 @ApiModel(description = "a User")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-19T16:14:14.335770107Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-20T09:20:19.743357111Z[Etc/UTC]")
 public class User {
   public static final String SERIALIZED_NAME_ID = "id";
   @SerializedName(SERIALIZED_NAME_ID)
--- src/main/java/com/cosmotech/client/model/UserWorkspace.java
+++ src/main/java/com/cosmotech/client/model/UserWorkspace.java
@@ -30,7 +30,7 @@ import java.util.List;
  * the list of User roles for a Workspace
  */
 @ApiModel(description = "the list of User roles for a Workspace")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-19T16:14:14.335770107Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-20T09:20:19.743357111Z[Etc/UTC]")
 public class UserWorkspace {
   public static final String SERIALIZED_NAME_ID = "id";
   @SerializedName(SERIALIZED_NAME_ID)
